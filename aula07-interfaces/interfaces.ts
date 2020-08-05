79e6335265b729706d8f25e08e49ac083e3c94f8
rayanepimentel <radharayg@gmail.com>
2020-08-05 11:10:21 -0300

    Interfaces e propriedades

--- aula07-interfaces/interfaces.ts
+++ aula07-interfaces/interfaces.ts
@@ -1,6 +1,8 @@
 
 interface Humano {
     nome: string
+    idade?: number //com ? eu estou dizendo que ele é opcional
+    [prop: string]: any //eu não sei o nome e nem o tipo
 }
 
 function saudarComOla(pessoa: Humano) {
@@ -21,3 +23,14 @@ mudarNome(pessoa)
 saudarComOla(pessoa)
 //nesse caso fica claro que temos uma duplicação  { nome: string }
 //alem de termos pssibilidade de usarmos type alias, podemos usar interfaces
+
+
+saudarComOla({nome: 'Jonas', idade: 27, xyz: true })
+//nesse contexto não é possivel, ele vai ficar reclamando que esse obj
+//não é derente a interface Humano, pq tem o atributo idade que não tá dentro de Humano
+//posso passar o atributo idade e dizer que ele é opcional
+//imagine que eu queira add um outro atributo que eu não sei o nome
+//saudarComOla({nome: 'Jonas', idade: 27, xyz: true }) xyz: true
+//nesse caso eu tbm tenhoa uma sintaxe para dizer que o nome é dinamico, o nome da propriedade, atributo do obj é dinamico
+//para isso usamos um par de [] -> [prop: string]: any
+//
