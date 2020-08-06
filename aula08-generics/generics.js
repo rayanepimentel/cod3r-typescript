92f5fc65a2257097bca8fe0db83380ac3f4e37a7
rayanepimentel <radharayg@gmail.com>
2020-08-06 13:22:06 -0300

    Tipos com any

--- aula08-generics/generics.js
+++ aula08-generics/generics.js
@@ -1,3 +1,12 @@
 "use strict";
 //Generics
+//uso de tipos mais genéricos, por exemplo any
+//quando vc usa any, acaba perdendo algumas verificações
+//ex:
+function echo(objeto) {
+    return objeto;
+}
+console.log(echo('João').length); //4
+console.log(echo(27).length); //não gera erro, mas retorna undefined
+console.log(echo({ nome: 'João', idade: 27 }).length); //undefined
 //# sourceMappingURL=generics.js.map
--- aula08-generics/generics.ts
+++ aula08-generics/generics.ts
@@ -1,1 +1,13 @@
 //Generics
+//uso de tipos mais genéricos, por exemplo any
+//quando vc usa any, acaba perdendo algumas verificações
+
+
+//ex:
+function echo(objeto: any) {
+    return objeto
+}
+
+console.log(echo('João').length) //4
+console.log(echo(27).length)//não gera erro, mas retorna undefined
+console.log(echo({nome: 'João', idade: 27}).length)//undefined
