88a742446cd0209a27b404648bd4a94dbaef8d67
rayanepimentel <radharayg@gmail.com>
2020-08-09 18:16:58 -0300

    Entendendo os Arquivos de Declaração de Tipos

--- aula10-BibliotecaJS/biblioteca.js
+++ aula10-BibliotecaJS/biblioteca.js
@@ -1,3 +1,8 @@
 "use strict";
+//opção simples usando declare
+// declare const $: any
 $('body').append('Usando jQuery');
+//segunda opção salvando arq. específico
+//arq. jquery.d.ts
+//e passar o declare para lá.
 //# sourceMappingURL=biblioteca.js.map
--- aula10-BibliotecaJS/biblioteca.ts
+++ aula10-BibliotecaJS/biblioteca.ts
@@ -1,6 +1,9 @@
 
 //opção simples usando declare
-declare const $: any
+// declare const $: any
 
 $('body').append('Usando jQuery')
 
+//segunda opção salvando arq. específico
+//arq. jquery.d.ts
+//e passar o declare para lá.
